08178b48df5ec753181ddd68b6f21ba82eafa17c
Brennan Gravanda <bgravanda3@gmail.com>
2024-03-26 13:13:43 -0400

    test #3

--- pokemon_v2/models.py
+++ pokemon_v2/models.py
@@ -1802,10 +1802,10 @@ class PokemonCries(HasPokemon):
     cries = models.JSONField()
 
 class MachineVersionLocation(HasLocation,HasLocationArea,Machine,HasName):
-    machine_number_id = models.IntegerField()
+    machine_number_id2 = models.IntegerField()
 
-    version_group_id = models.ForeignKey(
+    version_group_id2 = models.ForeignKey(
         VersionGroup, blank=True, null=True, on_delete=models.CASCADE
     )
 
-    move_id = models.ForeignKey(Move, blank=True, null=True, on_delete=models.CASCADE)
+    move_id2 = models.ForeignKey(Move, blank=True, null=True, on_delete=models.CASCADE)
