8c7050dc3b466bf01a9a182b896fbc6211f7d839
Ryan Blake <r1blake@student.bridgew.edu>
2024-04-25 10:03:24 -0400

    Changed machine_id back to machine and instead swapped machine_id in build for machine

--- pokemon_v2/models.py
+++ pokemon_v2/models.py
@@ -1839,4 +1839,4 @@ class GymLeaders(HasPokemon, HasMove, HasVersionGroup):
 
     name = models.CharField(max_length=10)
 
-    machine_id = CompositeForeignKey(Machine, null=False, to_fields={"id", "version_group_id"}, on_delete=models.CASCADE)
+    machine = CompositeForeignKey(Machine, null=False, to_fields={"id", "version_group_id"}, on_delete=models.CASCADE)
